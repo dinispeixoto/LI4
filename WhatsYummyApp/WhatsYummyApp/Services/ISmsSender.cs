759c67659c52be0ee79bab4ad5450b015319392b
Tres <marcelo-lima96@hotmail.com>
2017-05-26 00:21:30 +0100

    Adicionada asp.net core app

---
+++ WhatsYummyApp/WhatsYummyApp/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatsYummyApp.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
